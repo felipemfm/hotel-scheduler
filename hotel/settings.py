aa41b0b435848a2ec185fdc594a4d4ee469ea3ec
Felipe Machado <felipemfm@gmail.com>
2022-10-11 10:51:21 +0900

    Feat: Add postgres setting

--- hotel/settings.py
+++ hotel/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hotel',
+        'USER': 'postgres',
+        'PASSWORD': 'thepassword',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
