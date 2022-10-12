ff967cb00a50717270131ec02556fee543d30c49
Felipe Machado <felipemfm@gmail.com>
2022-10-12 15:09:13 +0900

    Feat: Add nested reservation nested urls

--- schedule/urls.py
+++ schedule/urls.py
@@ -1,11 +1,18 @@
+from cgitb import lookup
+from email.mime import base
 from rest_framework_nested import routers
 from . import views
 
 router = routers.DefaultRouter()
 router.register('hotels', views.HotelViewSet)
-router.register('reservation', views.ReservationViewSet)
+# router.register('reservation', views.ReservationViewSet)
 
 hotels_router = routers.NestedSimpleRouter(router, 'hotels', lookup='hotel')
 hotels_router.register('rooms', views.RoomViewSet)
+hotels_router.register('reservations', views.ReservationViewSet)
 
-urlpatterns = router.urls + hotels_router.urls
+rooms_router = routers.NestedDefaultRouter(
+    hotels_router, 'rooms', lookup='room')
+rooms_router.register('reservations', views.ReservationViewSet)
+
+urlpatterns = router.urls + hotels_router.urls + rooms_router.urls
