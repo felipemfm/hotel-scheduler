e3324aa0d48a7df37b10e3d7976905e5f4427bac
Felipe Machado <felipemfm@gmail.com>
2022-10-12 15:57:40 +0900

    Feat: Add authentication check for views

--- schedule/views.py
+++ schedule/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 from .models import Hotel, Reservation, Room
 from .serializer import HotelSerializer, ReservationSerializer, RoomSerializer
 
@@ -6,13 +8,16 @@ from .serializer import HotelSerializer, ReservationSerializer, RoomSerializer
 class HotelViewSet(ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class RoomViewSet(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class ReservationViewSet(ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
