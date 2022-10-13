2084f1e08b5cae2a34e981ed934a1e994ec3423c
Felipe Machado <felipemfm@gmail.com>
2022-10-13 12:14:40 +0900

    Feat: Add static path

--- hotel/urls.py
+++ hotel/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('schedule/', include('schedule.urls')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.jwt')),
