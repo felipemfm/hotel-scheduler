9abdafc89d3bdf3828ecc35213efd1ff0e746e23
Felipe Machado <felipemfm@gmail.com>
2022-11-13 15:18:59 +0900

    Feat: Add booking layout

--- frontend/src/layout/Booking.js
+++ frontend/src/layout/Booking.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HotelSelect from "../component/HotelSelect";
-
+import RoomSelect from "../component/RoomSelect";
 function Booking() {
   return (
     <div>
       <HotelSelect />
       <div className="row">
-        <div className="col"></div>
+        <div className="col">
+          <RoomSelect />
+        </div>
         <div className="col"></div>
       </div>
     </div>
